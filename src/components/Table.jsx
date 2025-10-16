b79c8bc40616cbf15b08a257f60c22df7a0fdd7b
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-10-16 10:35:35 +0100

    first commit

---
+++ src/components/Table.jsx
@@ -0,0 +1,59 @@
+import Pagination from './Pagination';
+import Loader from './Loader';
+
+const Table = ({columns, dataSource, pagination, loading=false}) => {
+    // console.log(dataSource);
+    return (
+        <div className='overflow-x-auto h-96'>
+            <table className='table table-pin-rows bg-base-100 tracking-wider font-libertinus'>
+                {/* head */}
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index} className='text-nowrap'>{column.title}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {loading ? (
+                        <tr>
+                            <td colSpan={columns.length} className='text-center'>
+                                <Loader size='md' />
+                            </td>
+                        </tr>
+                    ) : (
+                        dataSource.length > 0 ? (
+                            dataSource.map((row, rowIndex) => (
+                                <tr key={rowIndex} className='hover:bg-base-300'>
+                                    {columns.map((column, colIndex) => (
+                                        <td key={colIndex} className='text-nowrap'>{column.render? column.render({ row: { original: row } }) : row[column.dataId]}</td>
+                                    ))}
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan={columns.length} className='text-center'>No data found.</td>
+                            </tr>
+                        )
+                    )}
+                </tbody>
+                {pagination && dataSource.length > pagination.itemsPerPage && (
+                    <tfoot>
+                        <tr>
+                            <td colSpan={columns.length} className='text-center'>
+                                <Pagination
+                                    totalItems={pagination.totalItems}
+                                    itemsPerPage={pagination.itemsPerPage}
+                                    currentPage={pagination.currentPage}
+                                    onPageChange={pagination.onPageChange}
+                                />
+                            </td>
+                        </tr>
+                    </tfoot>
+                )}
+            </table>
+        </div>
+    );
+}
+
+export default Table;
