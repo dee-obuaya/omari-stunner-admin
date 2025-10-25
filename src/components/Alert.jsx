58bc9e1cabe2ac13d2f6bb8fe242f8ea620677af
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-10-25 15:55:53 +0100

    update motion transition styles

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+import { motion, AnimatePresence } from 'motion/react';
+
 const Alert = ({ type, message }) => {
     const getAlertClass = () => {
         switch (type) {
@@ -15,12 +18,19 @@ const Alert = ({ type, message }) => {
     };
 
     return (
-        <div role='alert' className={`alert alert-soft ${getAlertClass()} mb-4 w-1/2 flex justify-self-center`}>
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 shrink-0 stroke-current' fill='none' viewBox='0 0 24 24'>
-                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
-            </svg>
-            <span>{message || 'Hello!'}</span>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+                transition={{duration: 0.3, ease: 'easeInOut'}}
+                role='alert' className={`alert alert-soft ${getAlertClass()} mb-4 w-1/2 flex justify-self-center`}>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 shrink-0 stroke-current' fill='none' viewBox='0 0 24 24'>
+                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
+                </svg>
+                <span>{message || 'Hello!'}</span>
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
