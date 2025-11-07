b37773f0e8eb77b04dbb6e56f67a308b6ef5224c
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-11-07 15:31:52 +0100

    update reduce auto-hide timeout

--- src/contexts/AlertContext.jsx
+++ src/contexts/AlertContext.jsx
@@ -4,33 +4,33 @@ import Alert from '../components/Alert';
 const AlertContext = createContext();
 
 export const AlertProvider = ({ children }) => {
-  const [alert, setAlert] = useState({ type: '', message: '', visible: false });
+    const [alert, setAlert] = useState({ type: '', message: '', visible: false });
 
-  const showAlert = useCallback(({type, message}) => {
-    setAlert({ type: type, message: message, visible: true });
-    console.log('type: ', type);
-    console.log('message: ', message);
+    const showAlert = useCallback(({type, message}) => {
+        setAlert({ type: type, message: message, visible: true });
+        // console.log('type: ', type);
+        // console.log('message: ', message);
 
-    // Auto-hide after a few seconds
-    setTimeout(() => {
-      setAlert(prev => ({ ...prev, visible: false }));
-    }, 6000);
-  }, []);
+        // Auto-hide after a few seconds
+        setTimeout(() => {
+            setAlert(prev => ({ ...prev, visible: false }));
+        }, 4000);
+    }, []);
 
-  const hideAlert = useCallback(() => {
-    setAlert(prev => ({ ...prev, visible: false }));
-  }, []);
+    const hideAlert = useCallback(() => {
+        setAlert(prev => ({ ...prev, visible: false }));
+    }, []);
 
-  return (
-    <AlertContext.Provider value={{ showAlert, hideAlert }}>
-      {children}
-      {alert.visible && (
-        <div className='fixed top-36 left-1/2 -translate-x-1/2 z-9999 w-3/4'>
-            <Alert type={alert.type} message={alert.message} />
-        </div>
-      )}
-    </AlertContext.Provider>
-  );
+    return (
+            <AlertContext.Provider value={{ showAlert, hideAlert }}>
+            {children}
+            {alert.visible && (
+                <div className='fixed top-36 left-1/2 -translate-x-1/2 z-9999 w-3/4'>
+                    <Alert type={alert.type} message={alert.message} />
+                </div>
+            )}
+            </AlertContext.Provider>
+    );
 };
 
 export const useAlert = () => useContext(AlertContext);
