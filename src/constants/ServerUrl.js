b8685bcaa80952aefe6dad37f2ba5d5551b3faa5
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-11-22 15:12:05 +0100

    add table key constants

--- src/constants/ServerUrl.js
+++ src/constants/ServerUrl.js
@@ -3,3 +3,10 @@ export const API_BASE_URL = 'http://localhost:5000';
 export const LOGIN_PAGE = '/login';
 
 export const DASHBOARD = '/admin/home'
+
+// Table Keys
+export const IMAGES_TABLE_KEY = 'images-table';
+export const SERVICES_TABLE_KEY = 'services-table';
+export const USERS_TABLE_KEY = 'users-table';
+export const BOOKINGS_TABLE_KEY = 'bookings-table';
+export const TABS_TABLE_KEY = 'tabs-table';
